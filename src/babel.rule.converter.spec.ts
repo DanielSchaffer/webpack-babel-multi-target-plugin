673862de0cfcf030ef79d4a7f2919270114271cb
Daniel Schaffer <dan@dandoes.net>
2018-03-12 10:13:09 -0700

    set up unit testing

---
+++ src/babel.rule.converter.spec.ts
@@ -0,0 +1,11 @@
+import { BabelRuleConverter } from './babel.rule.converter';
+
+describe('BabelRuleConverter', () => {
+
+    it('is a test', () => {
+
+        new BabelRuleConverter();
+
+    });
+
+});
