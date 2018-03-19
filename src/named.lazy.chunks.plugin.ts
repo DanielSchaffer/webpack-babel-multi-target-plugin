24fb5cba5cc3e1e68a1d0fb2d9e182b56db6a7d9
Daniel Schaffer <dan@dandoes.net>
2018-03-19 15:57:34 -0700

    NamedLazyChunkPlugin - use entry module name if the chunk is the runtime chunk

--- src/named.lazy.chunks.plugin.ts
+++ src/named.lazy.chunks.plugin.ts
@@ -19,8 +19,18 @@ export class NamedLazyChunksPlugin implements Plugin {
             if (!group.origins) {
                 return;
             }
+            if (group.runtimeChunk === chunk) {
+                result.origins = [ group.runtimeChunk.entryModule.reasons[0].dependencies.originalName ];
+                result.isEntry = true;
+            }
             group.origins.forEach((origin: Origin) => {
                 if (!origin.request || !origin.request.match(/\.ngfactory$/)) {
+                    return;
+                }
+                if (!result.babelTarget) {
+                    result.babelTarget = BabelTarget.findTarget(origin.module);
+                }
+                if (result.isEntry) {
                     return;
                 }
                 const cleanedName = origin.request.replace(/\.ngfactory$/, '');
@@ -31,9 +41,6 @@ export class NamedLazyChunksPlugin implements Plugin {
                     result.origins.push(originName);
                 }
 
-                if (!result.babelTarget) {
-                    result.babelTarget = BabelTarget.findTarget(origin.module);
-                }
             });
             return result;
         }, { origins: [] } as { origins: string[], babelTarget?: BabelTarget });
@@ -49,13 +56,27 @@ export class NamedLazyChunksPlugin implements Plugin {
                 return;
             }
             compilation.hooks.beforeChunkIds.tap(PLUGIN_NAME, (chunks: Chunk[]) => {
+                const usedNames: { [name: string]: number } = {};
                 chunks.forEach(chunk => {
                     if (chunk.id || chunk.name) {
                         return;
                     }
-
                     const isVendorsChunk = chunk.chunkReason === 'split chunk (cache group: vendors)';
-                    chunk.id = `${isVendorsChunk ? 'vendors~': ''}${this.getNameFromOrigins(chunk)}`;
+                    let name = this.getNameFromOrigins(chunk);
+                    if (isVendorsChunk) {
+                        name = `vendors~` + name;
+                    }
+
+                    // HACK ALERT: the combination of multiple lazy child routes and chunk splitting can make this
+                    // get pretty hairy. Need to figure out a better way to handle it.
+                    if (typeof (usedNames[name]) === 'undefined') {
+                        usedNames[name] = -1;
+                    }
+                    usedNames[name]++;
+                    if (usedNames[name] > 0) {
+                        name += `.${usedNames[name]}`
+                    }
+                    chunk.id = name;
                 });
             });
         });
