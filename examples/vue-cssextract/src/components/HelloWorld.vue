9bdd96c9a683f06b2a80e76eae606059a29b61de
Rupert Angermeier <rupert.angermeier@gmail.com>
2018-10-09 11:42:30 +0200

    example using Vue with MiniCssExtractPlugin
    supports #4

---
+++ examples/vue-cssextract/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="greet">Open alert</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    greet() {
+      window.alert(`prop msg: ${this.msg}`)
+    }
+  }
+}
+</script>
+
+<style>
+.hello {
+  color: white;
+}
+</style>
