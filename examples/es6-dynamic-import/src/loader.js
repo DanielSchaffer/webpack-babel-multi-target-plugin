8abc3ada0e41d14345cf5e6832eb2fe1bc264083
George Griffiths <georgegriffiths@live.com>
2019-03-31 17:26:51 +0100

    Adjust example to cause normalize css issue

---
+++ examples/es6-dynamic-import/src/loader.js
@@ -0,0 +1,9 @@
+function importPluginByExpression(plugin) {
+    return import(`./plugins/${plugin}/plugin.js`)
+}
+
+export function loadPlugin() {
+    const pluginMatch = window.location.search.match(/\bplugin=([ab])\b/)
+    const plugin = pluginMatch ? pluginMatch[1] : 'a'
+    return importPluginByExpression(plugin)
+}
