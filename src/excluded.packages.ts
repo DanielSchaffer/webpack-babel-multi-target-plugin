42a784e78cc435af6fd833ce862c14438c6522bb
Daniel Schaffer <dan@dandoes.net>
2019-10-17 10:04:30 -0700

    fix: use OS-specific path separators for KNOWN_EXCLUDED and STANDARD_EXCLUDED patterns
    fixes #39

--- src/excluded.packages.ts
+++ src/excluded.packages.ts
@@ -1,21 +1,28 @@
+import { join } from 'path'
+
+
+function excludeNodeModulesPackage(...name: string[]): RegExp {
+  return new RegExp(join('node_modules', ...name))
+}
+
 // specific packages that aren't detected automatically, and are already es5
 export const KNOWN_EXCLUDED = [
-  /node_modules\/jsrsasign/,
+  excludeNodeModulesPackage('jsrsasign'),
 ]
 
 export const STANDARD_EXCLUDED = [
 
   // webpack add-ins and webpack loaders
-  /node_modules\/webpack/,
-  /node_modules\/([\w-_]+)-loader/, // required for css-loader, to-string-loader,
+  excludeNodeModulesPackage('webpack'),
+  excludeNodeModulesPackage('([\\w-_]+)-loader'), // required for css-loader, to-string-loader,
 
   // webpack dev server
-  /node_modules\/webpack-dev-server/,
-  /node_modules\/\(webpack\)-dev-server/,
+  excludeNodeModulesPackage('webpack-dev-server'),
+  excludeNodeModulesPackage('\\(webpack\\)-dev-server'),
 
   // polyfills
-  /node_modules\/@babel\/runtime/,
-  /node_modules\/core-js/,
-  /node_modules\/regenerator-runtime/,
+  excludeNodeModulesPackage('@babel', 'runtime'),
+  excludeNodeModulesPackage('core-js'),
+  excludeNodeModulesPackage('regenerator-runtime'),
 
 ]
